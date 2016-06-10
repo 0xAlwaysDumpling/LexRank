19bc721075e81a7be7c13282739fb6353e24702c
jtan25 <jhnnytny@gmail.com>
2016-06-10 04:02:51 +0000

    final commit

--- core/LexRank.py
+++ core/LexRank.py
@@ -1,22 +1,22 @@
 import string
 import sys
 import os
-
+import math
+import numpy as np
+import fnmatch
 sys.path.insert(0,'../vsm/')
 sys.path.insert(0,'../functions/')
 sys.path.insert(0,'../utils/')
 from utils import utils
 from vsm import tfidf
-import numpy as np
-import fnmatch
+
 
 
 class LexRank(object):
-	def __init__(self, docs_dir, sentence_threshold, ext='txt'):
+	def __init__(self, docs_dir, ext='txt'):
 		self.doc_dir = docs_dir
 		self.tfidf_scores = tfidf.getTFIDFScores(docs_dir, ext)
-		self.sentence_threshold = sentence_threshold
-		
+
 
 	def singleDocSummarization(self, doc_id, similarityFunct, normalizeFunct):
 		self.sentences = utils.getSentences(self.doc_dir,doc_id)
@@ -53,40 +53,118 @@ class LexRank(object):
 			matrix[x] = normalizeFunct(matrix[x], num_of_sentences)
 		return matrix
 	
+	def rank(self, similarity_threshold, epsilon, iterations, continuous, damp, multi=False):
+		if multi:
+			return multiDocRank(self.matrix,self.sentences, similarity_threshold, epsilon, iterations, continuous, damp)
+		else:
+			return singleDocRank(self.matrix, self.sentences, similarity_threshold, epsilon, iterations, continuous, damp)
+
+
+
+
+def multiDocRank(matrices, sentences, similarity_threshold, epsilon, iterations, continuous, damp):
+	rankings = []
+	for x in range(0, len(matrices)):
+		num_of_sentences = len(matrices[x][0])
+		similarity_matrix = reflectOverYX(matrices[x], num_of_sentences)
+		transition_matrix = buildTransitionMatrix(similarity_matrix, num_of_sentences, similarity_threshold, continuous)
+		rank,score = powerIteration(transition_matrix, num_of_sentences, epsilon, iterations, damp)
+		rankings.append(rank)
+	return rankings,sentences
+
+
+def singleDocRank(matrix, sentences, similarity_threshold, epsilon, iterations, continuous, damp):
+	num_of_sentences = len(matrix[0])
+	similarity_matrix = reflectOverYX(matrix, num_of_sentences)
+	transition_matrix = buildTransitionMatrix(similarity_matrix, num_of_sentences, similarity_threshold, continuous)
 	
-
-
-# class LexRank(object):
-# 	def __init__(self, doc_dir, sentence_threshold, doc_id = 1, ext='txt', num_of_sentences = 0, sentences = None):
-# 		self.tfidf_scores = tfidf.getTFIDFScores(doc_dir,ext)
-# 		self.doc_id = doc_id
-# 		self.sentence_threshold = sentence_threshold
-# 		if num_of_sentences > 0:
-# 			self.sentences = sentences
-# 			self.num_of_sentences = num_of_sentences
-# 		else:
-# 			self.sentences = utils.getSentences(doc_dir,doc_id)
-# 			self.num_of_sentences = len(self.sentences)
-# 		self.matrix = np.zeros((self.num_of_sentences,self.num_of_sentences))
+	rankings,score = powerIteration(transition_matrix, num_of_sentences, epsilon, iterations, damp)
+	return rankings,sentences
 
 
 
-# 	def buildSimilarityMatrix(self, similarityFunct, normalizeFunct):
-# 		for x in range(0,self.num_of_sentences):
-# 			vec_x = utils.mapTFIDFScores(self.sentences[x], self.tfidf_scores[self.doc_id])
-# 			for y in range(0,self.num_of_sentences):
-# 				if x == y:
-# 					self.matrix[x][y] = 1.0
-# 					continue
-# 				vec_y = utils.mapTFIDFScores(self.sentences[y], self.tfidf_scores[self.doc_id])
-# 				score = similarityFunct(vec_x, vec_y)
-# 				if np.isnan(score): score = 0.0
-# 				self.matrix[x][y] = score
 
-# 		#Normalize each row of scores
-# 		for x in range(0, self.num_of_sentences):
-# 			self.matrix[x] = normalizeFunct(self.matrix[x], self.num_of_sentences)
+def powerIteration(stochastic_matrix, num_of_sentences, epsilon, maxIterations, damp):
+    if damp > 0:
+       curr_matrix = applyDampingFactor(stochastic_matrix, num_of_sentences,damp).transpose()
+    else:
+        curr_matrix = stochastic_matrix.transpose()
+    curr_vec = np.zeros((num_of_sentences,1))
+    prev_vec = np.zeros((num_of_sentences,1))
+    curr_vec.fill(1.0/num_of_sentences)
 
+    for x in range(0,maxIterations):
+        prev_vec = curr_vec
+        curr_vec = np.dot(curr_matrix,curr_vec)
+        error = 0.0
+        for y in range(0, num_of_sentences):
+            error += math.sqrt(abs(curr_vec[y][0]-prev_vec[y][0]))
+        if error < math.sqrt(epsilon):
+            break
+
+    #Scores sorted from lowest to biggest
+    scores = [curr_vec[x][0] for x in range(0,num_of_sentences)]
+    
+
+    #Ranking based on biggest score to lowest so indexes are reversed
+    rankings = np.argsort(scores)
+    rankings = rankings[::-1]
+    return rankings,scores
+
+
+def applyDampingFactor(matrix, N, d):
+    #1-dB
+    matrix = np.multiply(matrix,1-d)
+    dU = np.zeros((N,N))
+    dU.fill(1.0/N)
+    return dU + matrix
+
+def findNeighbors(transition_matrix, num_of_sentences):
+    n_map = dict()
+    for x in range(0, num_of_sentences):
+        for y in range(0, num_of_sentences):
+            if transition_matrix[x][y] > 0:
+                if x in n_map:
+                    l = n_map[x]
+                    l.append(y)
+                else:
+                    l = []
+                    l.append(y)
+                    n_map[x] = l
+    return n_map
+
+
+
+
+#Build transition probability matrix with threshold | continuous
+def buildTransitionMatrix(similarity_matrix, num_of_sentences, similarity_threshold = .1, continuous=True):
+    transition_matrix = np.zeros((num_of_sentences,num_of_sentences))
+    for x in range(0,num_of_sentences):
+        degree = 0
+        for y in range(0, num_of_sentences):
+            if similarity_matrix[x][y] > similarity_threshold:
+                if continuous:
+                    transition_matrix[x][y] = similarity_matrix[x][y]
+                    degree += similarity_matrix[x][y]
+                else:
+                    transition_matrix[x][y] = 1
+                    degree += 1
+        for y in range(0,num_of_sentences):
+            transition_matrix[x][y] /= degree
+
+    return transition_matrix
+
+
+
+
+#Reflect Scores of the matrix over the line y = x b/c index order of sentence doesn't matter
+def reflectOverYX(matrix, num_of_sentences):
+    result = np.zeros((num_of_sentences, num_of_sentences))
+    for x in range(0,num_of_sentences):
+        for y in range(0,x+1):
+            result[x][y] = result[y][x] = matrix[x][y]
+    return result
+		
 
 
 
--- runLRExample.py
+++ runLRExample.py
@@ -6,23 +6,23 @@ from functions import similarity
 from functions import normalize
 
 
-#from functions import normalize, rank, similarity
-
 #DOC_NUM is 0 indexed
 DOC_PATH = './data/sample_dat/'
-SENTENCE_CUTOFF = 5
 DOC_NUM = 0
 EXT = 'txt'
-# lr = l.LexRank(DOC_PATH, SENTENCE_CUTOFF, DOC_NUM, EXT, 0, None)
-# lr.buildSimilarityMatrix(similarity.cosine, normalize.normalizeByLength)
+
+# rank our sentences
+cosine_threshold = 0.1
+epsilon = 0.005
+iterations = 100
+continuous_flag = True
+damping_factor = 0.15
 
 
 
-
-lr = l.LexRank(DOC_PATH, SENTENCE_CUTOFF, EXT)
+lr = l.LexRank(DOC_PATH, EXT)
 lr.singleDocSummarization(DOC_NUM, similarity.cosine, normalize.normalizeByLength)
+rankings, sentences = lr.rank(cosine_threshold, epsilon, iterations, continuous_flag, damping_factor, False)
 
-
-# lr = l.LR(DOC_PATH, SENTENCE_CUTOFF, EXT)
-# lr.multiDocSummarization(similarity.cosine, normalize.normalizeByLength)
-
+for s in rankings[0:5]:
+    print sentences[s]
